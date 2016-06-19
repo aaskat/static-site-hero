91e246e360d188c5ce2d7c4a72ddb40d4db20c1e
Askat <asanali08@gmail.com>
2016-06-19 16:30:10 +0600

    Added base for insert file link.

--- extension.js
+++ extension.js
@@ -1,6 +1,20 @@
+'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
+
+var insertText = (value) => {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showErrorMessage("Cant't insert text because no document is open.")
+        return;
+    }
+    let selection = editor.selection;
+    let range = new vscode.Range(selection.start, selection.end);
+    editor.edit((editBuilder) => {
+        editBuilder.replace(range, value);
+    });
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,18 +27,30 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let fileLinkDisposable = vscode.commands.registerCommand('extension.insertLink', () => {
+        vscode.window.showInformationMessage('Insert File Link Initiated');
+        let linkTypeList = ['File', 'Image'];
+
+        vscode.window.showQuickPick(linkTypeList, {
+            placeHolder: 'Link Type'
+        }).then(
+            result => {
+                insertText(result);
+            }
+        )
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(fileLinkDisposable);
+
+    let figureDisposable = vscode.commands.registerCommand('extension.insertFigure', () => {
+        vscode.window.showInformationMessage('Insert Figure Tag Initiated');
+    });
+
+    context.subscriptions.push(figureDisposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;

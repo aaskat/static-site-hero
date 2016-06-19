7de764840089ecdb3d7b32b2d8edce8076ef25a4
Askat <asanali08@gmail.com>
2016-06-19 18:08:35 +0600

    Changes readme file

--- extension.js
+++ extension.js
@@ -3,10 +3,18 @@
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 
+const figureTemplate = "<figure class=\"${figOptions.cssClass}\">\n" +
+    "![${figOptions.altText}](${figOptions.path}${figOptions.imageName})\n" +
+    "<figcaption>\n" +
+    "${figOptions.figCaption}\n" +
+    "</figcaption>\n" +
+    "</figure>";
+
+
 var insertText = (value) => {
     let editor = vscode.window.activeTextEditor;
     if (!editor) {
-        vscode.window.showErrorMessage("Cant't insert text because no document is open.")
+        vscode.window.showErrorMessage("Cant't insert text because no document is open.");
         return;
     }
     let selection = editor.selection;
@@ -20,8 +28,30 @@ let getImageTemplate = () => {
 }
 let getFileTemplate = () => {
     return vscode.workspace.getConfiguration("staticSiteHero")["filePathTemplate"];
-
 }
+
+let updateTemplateWithDate = (template) => {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = ('0' + (today.getMonth() + 1)).slice(-2);
+
+    template = template.replace("${year}", year);
+    template = template.replace("${month}", month);
+
+    return template;
+};
+
+let fillFigureTemplate = (figOptions) => {
+    figOptions.cssClass = figOptions.cssWidthClass + ' ' + figOptions.cssAlignmentClass;
+
+    let figure = figureTemplate.replace('${figOptions.imageName}', figOptions.imageName);
+    figure = figure.replace("${figOptions.path}", figOptions.path);
+    figure = figure.replace("${figOptions.altText}", figOptions.altText);
+    figure = figure.replace("${figOptions.figCaption}", figOptions.figCaption);
+    figure = figure.replace("${figOptions.cssClass}", figOptions.cssClass);
+    return figure;
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -44,9 +74,9 @@ function activate(context) {
             result => {
                 //insertText(result);
                 if (result === 'File') {
-                    insertText(getFileTemplate())
+                    insertText(updateTemplateWithDate(getFileTemplate()));
                 } else if (result === 'Image') {
-                    insertText(getImageTemplate())
+                    insertText(updateTemplateWithDate(getImageTemplate()));
                 }
             });
     });
@@ -54,7 +84,53 @@ function activate(context) {
     context.subscriptions.push(fileLinkDisposable);
 
     let figureDisposable = vscode.commands.registerCommand('extension.insertFigure', () => {
-        vscode.window.showInformationMessage('Insert Figure Tag Initiated');
+        let template = getImageTemplate();
+        template = updateTemplateWithDate(template);
+        let cssWidthClassList = vscode.workspace.getConfiguration("staticSiteHero")["widthCssClasses"];
+        let cssAlignmentClassList = vscode.workspace.getConfiguration("staticSiteHero")["alignmentCssClasses"];
+        let figOptions = {
+            imageName: '',
+            altText: '',
+            figCaption: '',
+            path: template,
+            cssWidthClass: '',
+            cssAlignmentClass: ''
+        };
+        vscode.window.showInputBox({
+            prompt: "Image File Name"
+        })
+            .then(value => {
+                figOptions.imageName = value;
+            })
+            .then(() => {
+                return vscode.window.showInputBox({
+                    prompt: "Figure Caption"
+                })
+                    .then(result => {
+                        figOptions.altText = result;
+                        figOptions.figCaption = result;
+                    });
+            })
+            .then(() => {
+                return vscode.window.showQuickPick(cssWidthClassList, {
+                    placeHolder: "Width Class"
+                })
+                    .then(result => {
+                        figOptions.cssWidthClass = result;
+                    });
+            })
+            .then(() => {
+                return vscode.window.showQuickPick(cssAlignmentClassList, {
+                    placeHolder: "Alignment Class"
+                })
+                    .then(result => {
+                        figOptions.cssAlignmentClass = result;
+                    })
+            })
+            .then(() => {
+                insertText(fillFigureTemplate(figOptions));
+            });
+
     });
 
     context.subscriptions.push(figureDisposable);
@@ -64,3 +140,5 @@ exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
+
+exports.updateTemplateWithDate = updateTemplateWithDate;
